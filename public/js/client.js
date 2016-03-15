0aef41480214832dc0e1446673d1b391b80174a2
Mike Dettmer <mike@mikedettmer.com>
2016-03-14 23:32:23 -0400

    removed unused code

--- public/js/client.js
+++ public/js/client.js
@@ -15,7 +15,6 @@ function fetchPizza(position) {
     for(var i = 0; i < businesses.length; i++) {
       var $newDiv = $('<div class="res">');
       $newDiv.append('<img src="'+businesses[i].image_url+'"><div class="info"><h3><a href="'+businesses[i].url+'">'+businesses[i].name+'</a></h3>'+'<div><a href="tel:'+businesses[i].phone+'">'+businesses[i].display_phone+'</a></div></div>');
-      //$newDiv.append('<img src="'+businesses[i].image_url+'">');
       $('#output').append($newDiv);
     }
   });
